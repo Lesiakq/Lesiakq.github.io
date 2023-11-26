9eb2eb95a9f3ec04093762e3f98fd965536c724c
Lesiakq <151232469+Lesiakq@users.noreply.github.com>
2023-11-26 12:04:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+//const cars = ["Saab", 4, "BMW"]
+
+
+
+
+
+const person = {
+    firstName: "Fiona",
+    lastName: "Lea",
+    age: 50,
+    eyeColor: "blue"
+    pets: 'dog'
+};
